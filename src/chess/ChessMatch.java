e2c062fd085225d17a8876fcfe1f76a91a5737f4
zaidancarvalho <felipecfernandez@hotmail.com>
2022-08-18 20:20:17 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -26,12 +25,16 @@ public class ChessMatch {
 		return mat; 
 	}
 	
-	//esse método vai ser responsável por iniciar a partida de xadrez
-	//colocando as peças no tabuleiro
+	//agora vai ser as coordenadas do sistema do xadrez e não no sistema da matriz
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
+	//agora está sendo passada as posições do xadrez
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
 
 	}
 }
---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,47 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+	
+	private char column;
+	private int row;
+	
+	public ChessPosition(char column, int row) {
+		if(column < 'a' || column > 'h' || row < 1 || row > 8) { //programação defensiva
+			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
+		}
+		this.column = column;
+		this.row = row;
+	}
+	
+
+	//foi apagado os sets porque não posso deixar que a coluna e a linha sejam livremente alteradas
+	public char getColumn() {
+		return column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+	
+	//retornar uma nova posição 
+	protected Position toPosition() {
+		return new Position(8 - row, column - 'a');
+	}
+	
+	//precisa me retornar a formula inversa do position
+	protected static ChessPosition fromPosition(Position position) {
+				//necessário realizar um casting para char porque o java não faz automaticamente
+		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow() );
+	}
+	
+	
+	//é colocado as aspas para forçar o compilador a enteder
+	//que isso é uma concateneção de strings
+	@Override
+	public String toString() {
+		return "" + column + row;
+	}
+	
+}
